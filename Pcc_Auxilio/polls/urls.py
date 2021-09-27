3defd79ecbc94e88080c379e81a2812a9d799475
MaelSantos <39337708+MaelSantos@users.noreply.github.com>
2021-09-27 15:37:01 -0300

    envio de informações entre views

--- Pcc_Auxilio/polls/urls.py
+++ Pcc_Auxilio/polls/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('cruzamento/', views.cruzamento, name='cruzamento'),
+    path('cruzamento/beneficiarios/', views.cruzar, name='cruzar'),
     # path('', views.BeneficiarioListView.as_view(), name='BeneficiarioList'),
     # path("<slug:slug>/", views.BeneficiarioDetailView.as_view(), name='BeneficiarioDetail')
 ]
