96c44ceab24fc58a141e62394ad01492356b7713
Mael Santos <maelsantos777@gmail.com>
2021-12-17 01:35:02 -0300

    correção configurações crawler heroku

--- polls/crawler/crawler.py
+++ polls/crawler/crawler.py
@@ -39,7 +39,7 @@ class Crawler:
 
         chrome_options.add_argument("start-maximized")
         chrome_options.add_argument("enable-automation")
-        chrome_options.add_argument("--headless")
+        # chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-infobars")
         chrome_options.add_argument("--disable-dev-shm-usage")
