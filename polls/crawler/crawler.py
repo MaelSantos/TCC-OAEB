2737e50b67708ea533978e4696440075c8b31112
Mael Santos <maelsantos777@gmail.com>
2021-12-17 01:57:37 -0300

    tentantiva de correção --headless 01

--- polls/crawler/crawler.py
+++ polls/crawler/crawler.py
@@ -45,9 +45,12 @@ class Crawler:
         chrome_options.add_argument("--disable-browser-side-navigation")
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--disable-extensions")
-        chrome_options.add_argument("no-sandbox")
-        chrome_options.add_argument("window-size=1920x1080")
+        chrome_options.add_argument("--proxy-server='direct://'")
+        chrome_options.add_argument("--proxy-bypass-list=*")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--window-size=1920x1080")
         chrome_options.add_argument("disable-dev-shm-usage")
+        chrome_options.add_argument('--ignore-certificate-errors')
 
         driver = webdriver.Chrome(ChromeDriverManager().install(),
                                   options=chrome_options)  ## caminho para o seu webdriver
