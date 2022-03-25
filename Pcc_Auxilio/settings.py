b6ca0f4165c9f404c0768b5d2ac22503c211a1c4
MaelSantos <39337708+MaelSantos@users.noreply.github.com>
2022-03-25 10:17:56 -0300

    Atualizando Heroku 2

--- Pcc_Auxilio/settings.py
+++ Pcc_Auxilio/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'NAME': 'teste',
         'USER': 'root',
         'PASSWORD': '',
-        'HOST': 'localhost',  # Or an IP Address that your DB is hosted on
+        'HOST': '127.0.0.1',  # Or an IP Address that your DB is hosted on
         'PORT': '3306',
     }
 }
